ffcd068c4e21b28c9e4339a4119517e718406167
Jin.Zitian <zitian.jin@faw-vw.in>
2020-12-22 12:40:11 +0800

    .

--- README.rst
+++ README.rst
@@ -15,7 +15,7 @@ To install:
 
 .. code-block:: bash
 
-    $ pip install lshash
+    $ pip install lshcos
 
 Quickstart
 ==========
@@ -24,16 +24,16 @@ To create band 2 row 6 hashes for input data of 8 dimensions:
 .. code-block:: python
 
     import numpy as np
-    from lsh import LSHCos
+    from lshcos import LSHCos
     
-    ### usage one：
+    ### usage one:
     lshcos = LSHCos(2, 6, 8)
     lshcos.add([1,2,3,4,5,6,7,8],'v_1')
     lshcos.add([2,3,4,5,6,7,8,9],'v_2')
     lshcos.add([10,12,99,1,5,31,2,3],'v_3')
     lshcos.query([1,2,3,4,5,6,7,7],2)
     
-    ### usage two：
+    ### usage two:
     lshcos = LSHCos(2, 6, 8)
     xxx = [('v_'+str(i),np.random.randn(8)) for i in range(100)]
     yyy = np.random.randn(8)
--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ except ImportError:
     from distutils.core import setup
 
 
-with open('README.rst') as f:
+with open('README.rst',encoding = 'utf-8') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
+with open('LICENSE.txt') as f:
     license = f.read()
 
 
@@ -28,9 +28,11 @@ setup(
     maintainer='Chef_J',
     maintainer_email='hedge_jzt@hotmail.com',
     description='A fast Python implementation of locality sensitive hashing with cosine distance.',
-    long_description=readme + '\n\n' + changes,
+    long_description=readme,
+    long_description_content_type='text/plain/markdown',
     license=license,
     requires=required,
+    url='https://github.com/jinzitian/LSHCos',
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
